c5dcbe153c58fe943d0a39e4ea9e5d1301f6aad5
Saahir777 <44057940+Saahir777@users.noreply.github.com>
2018-10-17 19:03:06 +0400

    test3

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -56,7 +56,7 @@ public class EchoApplication extends LineMessagingClientImpl {
 		
 		String originalMessageText = event.getMessage().getText();
 		String replyBotMessage = botReplies(originalMessageText);
-		final String followedUserId = event.getSource().getUserId();
+		/*final String followedUserId = event.getSource().getUserId();
 		
 		final LineMessagingClient client = LineMessagingClient
 		        .builder("KvPdRVx9Zhye3c74GhvJ2u6HtyUgJFuZSKU22wD8IfodRKhBsw4fdkSey0q/xsa/VuPBrA9shefDEn49yb4xo8Yy6sPF1izTfsgnfmm1aU4hrZgBOQasXMZwHvRdFcvupcGeFxZd1/JeVrWd6V54QwdB04t89/1O/w1cDnyilFU=")
@@ -71,8 +71,8 @@ public class EchoApplication extends LineMessagingClientImpl {
 		System.out.println(userProfileResponse.getUserId());
 		System.out.println(userProfileResponse.getDisplayName());
 		System.out.println(userProfileResponse.getPictureUrl());
-		
-		return new TextMessage(userProfileResponse.getDisplayName());
+		*/
+		return new TextMessage(replyBotMessage);
 	}
 
 	@EventMapping
