a7a3a673b0b17513e2f30bbaee4224f3845702f8
Saahir777 <44057940+Saahir777@users.noreply.github.com>
2018-10-17 21:06:34 +0400

    test3

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -41,7 +41,20 @@ import com.linecorp.bot.client.LineMessagingClientImpl;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.profile.UserProfileResponse;
 import java.util.concurrent.ExecutionException;
-import salesforce_rest.SendToSalesforce;
+import java.io.IOException;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.HttpStatus;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.JSONException;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -78,7 +91,11 @@ public class EchoApplication {
 		//System.out.println(userProfileResponse.getUserId());
 		//System.out.println(userProfileResponse.getDisplayName());
 		//System.out.println(userProfileResponse.getPictureUrl());
-		SendToSalesforce insertContact = new SendToSalesforce();
+		
+		
+		sendToSalesforce();
+		
+		
 		return new TextMessage(replyBotMessage);
 	}
 
@@ -104,4 +121,123 @@ public class EchoApplication {
 		}		
 		return botReplyMessage;
 	}
+	
+	public void sendToSalesforce() {
+		HttpClient httpclient = HttpClientBuilder.create().build();
+		 
+        // Assemble the login request URL
+        // Assemble the login request URL
+        String loginURL = LOGINURL +
+                          GRANTSERVICE +
+                          "&client_id=" + CLIENTID +
+                          "&client_secret=" + CLIENTSECRET +
+                          "&username=" + USERNAME +
+                          "&password=" + PASSWORD;
+ 
+        // Login requests must be POSTs
+        HttpPost httpPost = new HttpPost(loginURL);
+        HttpResponse response = null;
+ 
+        try {
+            // Execute the login POST request
+            response = httpclient.execute(httpPost);
+        } catch (ClientProtocolException cpException) {
+            cpException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+ 
+        // verify response is HTTP OK
+        final int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {
+            System.out.println("Error authenticating to Force.com: "+statusCode);
+            // Error is in EntityUtils.toString(response.getEntity())
+            return;
+        }
+ 
+        String getResult = null;
+        try {
+            getResult = EntityUtils.toString(response.getEntity());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+ 
+        JSONObject jsonObject = null;
+        String loginAccessToken = null;
+        String loginInstanceUrl = null;
+ 
+        try {
+            jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
+            loginAccessToken = jsonObject.getString("access_token");
+            loginInstanceUrl = jsonObject.getString("instance_url");
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+ 
+        baseUri = loginInstanceUrl + REST_ENDPOINT + API_VERSION ;
+        oauthHeader = new BasicHeader("Authorization", "OAuth " + loginAccessToken) ;
+        System.out.println("oauthHeader1: " + oauthHeader);
+        System.out.println("\n" + response.getStatusLine());
+        System.out.println("Successful login");
+        System.out.println("instance URL: "+loginInstanceUrl);
+        System.out.println("access token/session ID: "+loginAccessToken);
+        System.out.println("baseUri: "+ baseUri);        
+ 
+        // Run codes to query, isnert, update and delete records in Salesforce using REST API
+        //createLeads();
+        
+        createContact();
+ 
+        // release connection
+        httpPost.releaseConnection();
+	}
+	
+	// Create Contact using REST HttpPost
+    public void createContact() {
+        System.out.println("\n_______________ contact INSERT _______________");
+ 
+        String uri = baseUri + "/sobjects/Contact/";
+        try {
+ 
+            //create the JSON object containing the new contact details.
+            JSONObject contact = new JSONObject();
+            contact.put("FirstName", "James");
+            contact.put("LastName", "Bale");
+ 
+            System.out.println("JSON for contact record to be inserted:\n" + contact.toString(1));
+ 
+            //Construct the objects needed for the request
+            HttpClient httpClient = HttpClientBuilder.create().build();
+ 
+            HttpPost httpPost = new HttpPost(uri);
+            httpPost.addHeader(oauthHeader);
+            httpPost.addHeader(prettyPrintHeader);
+            // The message we are going to post
+            StringEntity body = new StringEntity(contact.toString(1));
+            body.setContentType("application/json");
+            httpPost.setEntity(body);
+ 
+            //Make the request
+            HttpResponse response = httpClient.execute(httpPost);
+ 
+            //Process the results
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == 201) {
+                String response_string = EntityUtils.toString(response.getEntity());
+                JSONObject json = new JSONObject(response_string);
+                // Store the retrieved contact id to use when we update the contact.
+                contactId = json.getString("id");
+                System.out.println("New contact id from response: " + contactId);
+            } else {
+                System.out.println("Insertion unsuccessful. Status code returned is " + statusCode);
+            }
+        } catch (JSONException e) {
+            System.out.println("Issue creating JSON or processing results");
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (NullPointerException npe) {
+            npe.printStackTrace();
+        }
+    } 
 }
